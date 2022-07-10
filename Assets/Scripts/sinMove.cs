139f630b60b025c3608156eced685d489305ff33
kusamoti <raren0130@gmail.com>
2022-07-10 13:01:31 +0900

    Logsプレファブの丸太の数を追加
    Logsプレファブの丸太の数を追加
    SinMoveスクリプトを現在の位置からサインで移動するように変更

--- Assets/Scripts/sinMove.cs
+++ Assets/Scripts/sinMove.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class sinMove : MonoBehaviour
 {
-    public bool isMove = false;
-    public float amp = 2.0f;
+    public bool isMove = true;
+    public float amp = 1.0f;
     public float rate = 1.0f;
-    private float time = 0;
+    private float origintime = 0;
+    private Vector3 initPos;
+
+    private void Start()
+    {
+        initPos = transform.position;
+    }
 
     void Update()
     {
         if (isMove)
         {
-            transform.position = new Vector3(amp * Mathf.Sin(rate * time), transform.position.y, 0);
-            time += Time.deltaTime;
+            transform.position = initPos + new Vector3(amp * Mathf.Sin(rate * origintime), 0, 0);
+            origintime += Time.deltaTime;
         }
 
     }
